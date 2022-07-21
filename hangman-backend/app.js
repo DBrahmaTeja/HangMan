08b5fb89979830b4eb46e5f5fad43f9c02c9f634
BrahmaTeja <80442374+DBrahmaTeja@users.noreply.github.com>
2022-07-21 20:01:56 +0530

    Code Added.

---
+++ hangman-backend/app.js
@@ -0,0 +1,30 @@
+const Express = require('express');
+const Router = require('./routes');
+const { sequelize, Word } = require('./models');
+
+
+async function initialize() {
+  const app = Express();
+  
+  app.use(Express.json());
+  app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+    res.set("Access-Control-Allow-Headers", "Content-type")
+    next();
+  })
+  app.use("/api", Router);
+
+  app.get("/cookie", (req, res) => {
+    res.setHeader("Set-Cookie", "name=BrahmaTeja")
+
+    res.send("Hello World")
+  })
+
+  await sequelize.sync();
+
+  app.listen(8000, () => {
+    console.log("Running application on port 8000")
+  })
+}
+
+initialize();
